f489b12919748a3e5a1e6633784184df449d7875
Sergey Vasketsov <vasketsov@mail.ru>
2012-02-17 17:49:34 +0400

    sasgis 1178: добавлено игнорирование ненужных ошибок при загрузке DLL для xerces

--- PUBLIC/vsagps_public_xml_dom.pas
+++ PUBLIC/vsagps_public_xml_dom.pas
@@ -11,6 +11,7 @@ unit vsagps_public_xml_dom;
 interface
 
 uses
+  Windows, // for SEM_FAILCRITICALERRORS and other constants
   SysUtils,
 {$if defined(VSAGPS_USE_SOME_KIND_OF_XML_IMPORT)}
   xmldom,
@@ -41,6 +42,65 @@ function VSAGPS_Load_DOMDocument_FromStream(const ADOMDocument: IDOMDocument; co
 
 implementation
 
+type
+  //TGetErrorModeProc = function: UINT; stdcall;
+  TNtQueryInformationProcessProc = function(hProcess: THandle;
+                                            ProcessInformationClass: Byte;
+                                            ProcessInformation: Pointer;
+                                            ProcessInformationLength: ULONG;
+                                            ReturnLength: PULONG): LongInt; stdcall;
+
+  TNtSetInformationProcessProc = function(hProcess: THandle;
+                                          ProcessInformationClass: Byte;
+                                          ProcessInformation: Pointer;
+                                          ProcessInformationLength: ULONG): LongInt; stdcall;
+
+// same as GetErrorMode but not only on vista
+function CallGetErrorMode(var AObtainedOk: Boolean): UINT;
+var p: Pointer;
+begin
+  (*
+  p:=GetProcAddress(GetModuleHandle('kernel32.dll'),'GetErrorMode');
+  if (nil<>p) then begin
+    // default (vista only)
+    Result:=TGetErrorModeProc(p);
+    AObtainedOk:=TRUE;
+    Exit;
+  end;
+  *)
+
+  p:=GetProcAddress(GetModuleHandle('ntdll.dll'),'NtQueryInformationProcess');
+  if (nil<>p) then begin
+    // ProcessDefaultHardErrorMode - from NT build 1381
+    if (0=TNtQueryInformationProcessProc(p)(GetCurrentProcess, $C, @Result, sizeof(Result), nil)) then begin
+      AObtainedOk:=TRUE;
+      Exit;
+    end;
+  end;
+
+  Result:=0;
+end;
+
+procedure CallSetErrorMode(const AMode: UINT);
+var p: Pointer;
+begin
+  p:=GetProcAddress(GetModuleHandle('ntdll.dll'),'NtSetInformationProcess');
+  if (nil<>p) then begin
+    // ProcessDefaultHardErrorMode - from NT build 1381
+    TNtSetInformationProcessProc(p)(GetCurrentProcess, $C, @AMode, sizeof(AMode));
+  end;
+end;
+
+function AdjustErrorMode(const AMode: UINT): UINT;
+begin
+  // The hard error mode is a bit array of flags that correspond to the flags
+  // used by the Win32 function SetErrorMode, with the exception that
+  // the meaning of the lowest bit is inverted.
+  Result := AMode or SEM_NOOPENFILEERRORBOX;
+  if (Result and SEM_FAILCRITICALERRORS)<>0 then
+    Result := Result - SEM_FAILCRITICALERRORS;
+end;
+
 {$if defined(VSAGPS_USE_SOME_KIND_OF_XML_IMPORT)}
 function VSAGPS_Create_DOMDocument(var ADOMDocument: IDOMDocument; const ARaiseErrorIfFALSE: Boolean; const ASetValidationMode: Byte): Boolean;
 
@@ -58,13 +118,25 @@ function VSAGPS_Create_DOMDocument(var ADOMDocument: IDOMDocument; const ARaiseE
     except
     end;
   end;
+
+var
+  VErrorMode: UINT;
+  VCanAdjustErrorMode: Boolean;
 begin
   Result:=FALSE;
 
 {$if defined(VSAGPS_USE_XERCES_XML_IMPORT)}
   if (not Assigned(ADOMDocument)) then
   try
-    ADOMDocument:=XercesDOM.DOMImplementation.createDocument('','',nil);
+    VErrorMode:=CallGetErrorMode(VCanAdjustErrorMode);
+    if VCanAdjustErrorMode then
+      CallSetErrorMode(AdjustErrorMode(VErrorMode));
+    try
+      ADOMDocument:=XercesDOM.DOMImplementation.createDocument('','',nil);
+    finally
+      if VCanAdjustErrorMode then
+        CallSetErrorMode(VErrorMode);
+    end;
     Inc(Result);
   except
   end;
